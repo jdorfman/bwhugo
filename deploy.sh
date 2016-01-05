4062e4baff8aa6940cf3316aea5b722f37e5c251
Justin Dorfman <justindorfman@gmail.com>
2016-01-05 15:35:38 -0800

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+source $(which shml) # https://shml.xyz
+
+echo $(bgcolor green) "Deploying updates to GitHub..." $(bgcolor end)
+
+# Build the project.
+hugo
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+git subtree push --prefix=public git@github.com:jdorfman/bwhugo.git gh-pages
